7a68fc48aaca4a468b54de5a3acf9b4bb379709b
ZeNoobi <ctaaseen@gmail.com>
2023-10-18 11:03:03 -0700

    BankerAlgorithm

---
+++ TCBanker.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+    bool menu = true;
+
+    while (menu == true)
+    {
+        printf("");
+    }
+    
+
+    return 1;
+}
