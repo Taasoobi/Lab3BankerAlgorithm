fee9a3b053e0fff15ecb329f2914a8e0028f1688
Taasoobi <ctaaseen@gmail.com>
2023-10-30 18:11:57 -0700

    Created 2D dynamic resource array

--- TCBanker.c
+++ TCBanker.c
@@ -2,12 +2,63 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+int p;
+int r;
+
+int *resource = NULL;
+int *available = NULL;
+int *max = NULL;
+int *allocated = NULL;
+int *need = NULL;
+
+
+void params(){
+int rUnit;
+
+printf("\n Enter number of processes: ");
+scanf("%d", &p);
+printf("\n Enter number of resources: ");
+scanf("%d", &r);
+resource = malloc((p * r) * sizeof(int));
+
+printf("Enter number of units for resources (r0 to r%d): ", r-1);
+for (int i = 0; i < r; i++){
+    scanf("%d", &rUnit);
+    resource[i] = rUnit;
+    //available[i] = rUnit;
+}
+
+printf("\nunits for resources:");
+for(int i = 0; i<r; i++){
+printf("%d ", resource[i]);
+}
+}
+
 int main(){
     bool menu = true;
+    int menuop;
 
     while (menu == true)
     {
-        printf("");
+        printf("\nBanker's Algorithm\n--------------------");
+        printf("\n1)Enter Parameters\n2)Determine Safe Sequence\n3)Quit Program\n");
+        scanf("%d", &menuop);
+
+        if (menuop == 1)
+        {
+            printf("1 Chosen\n");
+            params();
+        } else if (menuop == 2)
+        {
+            printf("2 Chosen\n");
+        } else if (menuop == 3)
+        {
+            printf("Program Successfully Quit.\n");
+            menu = false;
+        } else {
+            printf("Error: Enter a number 1-3");
+        }
+        
     }
     
 
